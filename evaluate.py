ac2c7d8b29019233a018d22306d97d4f719178c5
felipebravom <admin@weka1.resnet.cms.waikato.ac.nz>
2017-01-27 13:46:20 +1300

    New evaluation script

--- evaluate.py
+++ evaluate.py
@@ -13,7 +13,7 @@
 
 # tweets_to_arff.py
 # felipebravom
-# Running example: python evaluate.py data/wekapredictions.csvfixed data/anger-ratings-0to1.test.gold.tsv
+# Running example: python evaluate.py data/wekapredictionsfixed.csv data/anger-ratings-0to1.test.gold.tsv
 
 import numpy
 import sys
@@ -30,33 +30,62 @@ def evaluate(pred,gold):
     f.close()
     
 
-    if(len(pred_lines)==len(gold_lines)):
-      
+    if(len(pred_lines)==len(gold_lines)):       
+        # align tweets ids with gold scores and predictions
+        data_dic={}
         
-        pred_scores=[]
-        gold_scores=[]
-        
-        for line in pred_lines:
-            parts=line.split('\t')
-            if len(parts)==4:     
-                  pred_scores.append(float(line.split('\t')[3]))
-            else:
-                raise ValueError('Format problem.')
-            
-            
         for line in gold_lines:
             parts=line.split('\t')
             if len(parts)==4:   
-                gold_scores.append(float(line.split('\t')[3]))
+                data_dic[int(parts[0])]=[float(line.split('\t')[3])]
+            else:
+                raise ValueError('Format problem.')
+        
+        
+        for line in pred_lines:
+            parts=line.split('\t')
+            if len(parts)==4:  
+                if int(parts[0]) in data_dic:
+                    try:
+                        data_dic[int(parts[0])].append(float(line.split('\t')[3]))
+                    except ValueError:
+                        # Invalid predictions are replaced by a default value
+                        data_dic[int(parts[0])].append(0.5)
+                else:
+                    raise ValueError('Invalid tweet id.')
             else:
                 raise ValueError('Format problem.')
             
+            
+        gold_scores=[]  
+        pred_scores=[]
+         
+            
+        for id in data_dic:
+            if(len(data_dic[id])==2):
+                gold_scores.append(data_dic[id][0])
+                pred_scores.append(data_dic[id][1])
+            else:
+                raise ValueError('Repeated id in test data.')
+                
+      
         
-        pears_corr=numpy.corrcoef(pred_scores, gold_scores)[0, 1]
+        cov=numpy.cov(pred_scores, gold_scores)[0, 1]
+        sd_pred=numpy.std(pred_scores)
+        #Replace zero standard deviation values
+        sd_pred= sd_pred if sd_pred != 0 else 1
+        
+        sd_gold=numpy.std(gold_scores)
+        sd_gold= sd_gold if sd_gold != 0 else 1
+
+        pears_corr=cov/(sd_pred*sd_gold)
+        
         print "Pearsons correlation\t"+str(pears_corr)+"\n"
+                                    
+                          
         
     else:
-        raise ValueError('Predictions and gold data have different number of lines')
+        raise ValueError('Predictions and gold data have different number of lines.')
 
 def main(argv):
     pred=argv[0]
